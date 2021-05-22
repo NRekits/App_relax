aa22e1fbdd43759a771859cf22cdf2f82f3f2135
Victor Hugo Olivetti Alvarez <victoralvarez.vhoa@gmail.com>
2021-05-22 00:50:15 -0500

    Cascaron de lista de meditaciones y meditaciones al 50%
    Ya esta completado el cascaron de la lista de meditaciones

--- Relax/screens/CatmedScreen.js
+++ Relax/screens/CatmedScreen.js
@@ -10,26 +10,41 @@ import {
 } from 'native-base';
 
 const LISTA_MEDITACIONES = [
+
     {
         id: 1,
-        nombre: "kkdvk",
-        descripcion: "Esto es solo una prueba de la lista"
-    }
+        nombre: "Desayuno consciente",
+        descripcion: "La misma práctica de mindfulness puede continuar en el desayuno. Deja el móvil mientras estás comiendo y aprovecha estos 10 minutos para centrarte en los alimentos que estás comiendo, su sabor, olor... en todo lo que haces: untar las tostadas, remover el café... Te sorprenderá esta nueva manera de desayunar."
+    },
+
+    {
+        id: 2,
+        nombre: "Respiración de aterrizaje aquí y ahora",
+        descripcion: "Este ejercicio es ideal para para apagar el piloto automático. Al practicarlo, la atención se centra en el momento presente y detiene el fluir constante de los pensamientos, recuerdos, imágenes o ideas. Es ideal para descargar de la tensión acumulada de una manera muy sencilla. \
+        Para llevarlo a cabo, es necesario centrar la atención en la respiración. Se debe realizar una inspiración suave, profunda y constante por la nariz. Al llenarnos de aire, soltar enseguida el aire por la boca con intensidad pero sin forzar la garganta. Al notar una distracción (que es normal), observamos qué es aquello que captó nuestra atención y regresamos de nuevo a la respiración."
+    },
 ];
 
-const Item = (props) => {
-    console.log(props);
-    return (
-        <ListItem>
-            <Text>{props.nombre}</Text>
-        </ListItem>
-    );
-}
 
 export default class ListaMeditacionesScreen extends React.Component {
-    render(){
+    constructor(props){
+        super(props);
+    }
+    Item = (propis) => {
         return (
-            <Container>
+            <ListItem button style={{ borderBottomWidth: 1 }} onPress={() => this.props.navigation.navigate('meditacion', {nombre: propis.nombre, descripcion: propis.descripcion})}>
+                <Body>
+                    <Text>{propis.nombre}</Text>
+                </Body>
+                <Right>
+                    <Icon name="arrow-forward" />
+                </Right>
+            </ListItem>
+        );
+    }
+    render() {
+        return (
+            <Container >
                 <Header>
                     <Left>
                         <Button>
@@ -40,7 +55,7 @@ export default class ListaMeditacionesScreen extends React.Component {
                         <Title>Header</Title>
                     </Body>
                 </Header>
-                <List dataArray={LISTA_MEDITACIONES} keyExtractor={(item, index) => index.toString()} renderRow={Item} />
+                <List dataArray={LISTA_MEDITACIONES} keyExtractor={(item, index) => index.toString()} renderRow={this.Item} ></List>
                 <Footer>
                     <FooterTab>
                         <Button full>
