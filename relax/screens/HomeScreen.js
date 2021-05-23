0eb123ee4a318799e1120d6721f3fb8b34f7e1bf
NRekits <64814187+NRekits@users.noreply.github.com>
2021-05-22 21:10:38 -0500

    Footer inicio

--- relax/screens/HomeScreen.js
+++ relax/screens/HomeScreen.js
@@ -140,14 +140,84 @@ class HomeScreen extends React.Component {
             </View>
         </Content>
         <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
+         
         </Footer>
       </Container>
     );
   }
 }
 export default HomeScreen;
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    flexDirection:'column',
+    alignItems: "stretch",
+    justifyContent: "center",
+    padding: 20,
+    fontFamily: "Dosis",
+    color: "white",
+  },
+  Text: {
+    fontSize: 40,
+  },
+  Text2: {
+    marginTop: 5,
+    fontWeight:'400',
+    fontSize:20,
+    color: "white",
+    marginLeft: 5,
+    fontFamily: "Dosis",
+  },
+  Text3: {
+    marginTop: 10,
+    fontSize: 15,
+    color: "#C4EFFF",
+    marginLeft: 5,
+    fontFamily: "Dosis",
+  },
+  Image: {
+    alignSelf: "center",
+    marginBottom:10
+  },
+  Input: {
+    
+    alignSelf: "flex-start",
+    color: "white",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: 20,
+    marginRight: 5,
+  },
+  Button: {
+    alignSelf:'center',
+    marginTop: 20,
+  }, 
+  Label:{
+    color: "white",
+    fontFamily: "Dosis",
+    fontWeight:'400',
+    fontSize:20,
+    marginRight:5,
+    marginBottom: 10,
+ 
+  },
+  Item:{
+    padding:5,
+    marginTop:30,
+  },
+  H1:{
+    alignSelf: "center",
+    color: "white",
+    fontFamily: "Dosis",
+    fontWeight:'400',
+    fontSize:30,
+    marginTop:20
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: windowHeight,
+  }
+});
