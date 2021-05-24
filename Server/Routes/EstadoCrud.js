1659ef3ab3ddb59869bd29996162c5651b679d55
Victor Hugo Olivetti Alvarez <victoralvarez.vhoa@gmail.com>
2021-05-23 19:34:47 -0500

    No funiciona la peticion

--- Server/Routes/EstadoCrud.js
+++ Server/Routes/EstadoCrud.js
@@ -1,39 +1,37 @@
 const router = require('express').Router();
-const Estado =require('../models/Estado')
-
-
+const Estado = require('../models/Estado')
 
 router.post('/insertar', (req, res) => {
-   
-        const estado = new Estado({
-            iduser:req.body.iduser,
-            nombre: req.body.nombre,
-            descripcion:req.body.descripcion,
-            fecha: req.body.fecha
-         
-        });
 
-        estado.save()
-        .then(doc=>{
-            console.log('dato insertado',doc)
-            res.json({response:'exito'})
+    const estado = new Estado({
+        iduser: req.body.iduser,
+        nombre: req.body.nombre,
+        descripcion: req.body.descripcion,
+        fecha: req.body.fecha
+
+    });
+
+    estado.save()
+        .then(doc => {
+            console.log('dato insertado', doc)
+            res.json({ response: 'exito' })
         })
-        .catch (err=>{
+        .catch(err => {
             console.log("error al insertar", err.message)
-        }) 
-
-    
         })
+
+
+})
 
 
 router.get('/Estadopormes/id:/mes:', (req, res) => {
-    const id= req.params.id
-    const mes= req.params.mes
+    const id = req.params.id
+    const mes = req.params.mes
     try {
-       
 
-    }catch(e){
-        return status(400).json({error: "Hubo un error en el login, por favor intenta de nuevo"})
+
+    } catch (e) {
+        return status(400).json({ error: "Hubo un error en el login, por favor intenta de nuevo" })
     }
 
 
