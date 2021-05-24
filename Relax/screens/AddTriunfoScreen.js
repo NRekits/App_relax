9e7b300d7cc8456994c3de8277e984fcb5ab614e
Victor Hugo Olivetti Alvarez <victoralvarez.vhoa@gmail.com>
2021-05-24 09:43:57 -0500

    Se ingreso el modal

--- Relax/screens/AddTriunfoScreen.js
+++ Relax/screens/AddTriunfoScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Dimensions, Alert, Image, StyleSheet } from "react-native";
 import {
-    Container, Content,
-    Header, Left,
-    Form, Text,
-    Button, Body,
-    Icon, Title,
-    Item, Input,
-    Textarea,
-    Label,
+  Container, Content,
+  Header, Left,
+  Form, Text,
+  Button, Body,
+  Icon, Title,
+  Item, Input,
+  Textarea,
+  Label
 } from 'native-base';
-import {LoadingFull} from './../Components/Loading';
-import {LinearGradient} from 'expo-linear-gradient';
+import Modal from 'react-native-modalbox';
+import { LoadingFull } from './../Components/Loading';
+import { LinearGradient } from 'expo-linear-gradient';
 import IP_DB from './../ip_address';
 
 export default class AddTriunfoScreen extends React.Component {
@@ -23,12 +24,13 @@ export default class AddTriunfoScreen extends React.Component {
       descripcion: "",
       isLoading: true,
       isModification: false,
-      error: false
+      error: false,
+      isDisabled: false
     };
   }
   goBack = () => {
     this.props.navigation.goBack();
-   }
+  }
   componentDidMount() {
     this.setState({ idUsuario: this.props.route.params.idUsuario, isLoading: false });
   }
@@ -52,18 +54,21 @@ export default class AddTriunfoScreen extends React.Component {
         fecha: today.toISOString()
       })
     })
-    .then((res) => res.json())
-    .then(() => {this.setState({error: false})})
-    .catch((error) => {
-      console.error(error);
-      this.setState({error: true});
-    })
-    .finally(() => {
-      if(!this.state.error){
-        Alert.alert('Felicidades', "Te ganaste una medalla", [{text: "Muchas gracias"}])
-        this.props.navigation.navigate('Home');
-      }
-    });
+      .then((res) => res.json())
+      .then(() => { this.setState({ error: false }) })
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error: true });
+      })
+      .finally(() => {
+        if (!this.state.error) {
+          this.refs.modalTriunfo.open()
+        }
+      });
+  }
+
+  closeModal() {
+    this.props.navigation.navigate('Home');
   }
 
   onChangeTime = (event, date) => {
@@ -87,27 +92,37 @@ export default class AddTriunfoScreen extends React.Component {
     }
     if (isLoading) {
       return (
-          <LoadingFull />
+        <LoadingFull />
       );
     } else {
-    console.log(this.state);
+      console.log(this.state);
       return (
         <Container>
-              <LinearGradient
-          // Background Linear Gradient
-          colors={["#00B0E8", "#BB8FCE"]}
-          style={styles.background}
-        />
+          <LinearGradient
+            // Background Linear Gradient
+            colors={["#00B0E8", "#BB8FCE"]}
+            style={styles.background}
+          />
+          <Modal
+            onClosed={this.closeModal.bind(this)}
+            ref={"modalTriunfo"}
+            position={"center"}
+            isDisabled={this.state.isDisabled}
+            style={styles.modal}
+          >
+            <Text>Holis</Text>
+            <Button onPress={() => { this.setState({isDisabled: !this.state.isDisabled})}}><Text>Continuar</Text></Button>
+          </Modal>
           <Header transparent androidStatusBarColor="#00B0E8">
             <Body>
               <Title style={styles.Header}>
                 {" "}
                 <Button iconRight transparent >
-              <Icon
-                name="arrow-back"
-                style={{ color: "white" }}
-                onPress={this.goBack}
-              /></Button>{" "}
+                  <Icon
+                    name="arrow-back"
+                    style={{ color: "white" }}
+                    onPress={this.goBack}
+                  /></Button>{" "}
                 {Action} Triunfo
               </Title>
             </Body>
@@ -118,7 +133,7 @@ export default class AddTriunfoScreen extends React.Component {
               <Item floatingLabel style={styles.Item}>
                 <Label style={styles.Label}>¿Qué triunfo cumpliste hoy?</Label>
                 <Input
-                    style={styles.Input}
+                  style={styles.Input}
                   value={nombre}
                   onChangeText={(text) => {
                     this.setState({ nombre: text });
@@ -136,10 +151,10 @@ export default class AddTriunfoScreen extends React.Component {
                 style={styles.Textarea}
                 placeholder="Describe tu triunfo..."
               />
-             
+
             </Form>
 
-            <Button block rounded style={styles.Button} onPress={() => {this.addTriunfo()}}>
+            <Button block rounded style={styles.Button} onPress={() => { this.addTriunfo() }}>
               <Text>{Action}</Text>
             </Button>
           </Content>
@@ -150,81 +165,84 @@ export default class AddTriunfoScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    Content: {
-      marginTop: 40,
-    },
-    Container: {
-      flex: 1,
-      flexDirection: "column",
-      alignItems: "stretch",
-      justifyContent: "center",
-      fontFamily: "Dosis",
-      color: "white",
-      padding:10,
-    },
-    Item: {
-      marginTop: 30,
-      padding: 10,
-    },
-    Input: {
-      alignSelf: "flex-start",
-   
-      fontFamily: "Dosis",
-      fontWeight: "400",
-      fontSize: 20,
-      marginRight: 5,
-    },
-    Label: {
-      color: "#BB8FCE",
-      fontFamily: "Dosis",
-      fontWeight: "400",
-      fontSize: 20,
-      padding: 10,
-      marginBottom: 0,
-    },
-    background: {
-      position: "absolute",
-      left: 0,
-      right: 0,
-      top: 0,
-      height: 100,
-    },
-    Header: {
-      color: "#C4EFFF",
-      fontFamily: "Dosis",
-      fontSize: 40,
-      fontWeight: "600",
-      alignSelf: "flex-start",
-    },
-    H2: {
-      textAlign: "center",
-      color: "#BB8FCE",
-      fontFamily: "Dosis",
-      fontWeight: "400",
-      fontSize: 25,
-      marginTop: 5,
-    },
-    Button: {
-      alignSelf: "center",
-      backgroundColor: "#BB8FCE",
-      marginTop: 20,
-    },
-    Text: {
-      textAlign: "center",
-      color: "#BB8FCE",
-      fontFamily: "Dosis",
-      fontWeight: "400",
-      fontSize: 25,
-      marginTop: 5,
-    },
-    Textarea: {
-      textAlign: "left",
-      fontFamily: "Dosis",
-      fontWeight: "400",
-      borderRadius:20,
-      fontSize: 20,
-      padding:5,
+  Content: {
+    marginTop: 40,
+  },
+  Container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center",
+    fontFamily: "Dosis",
+    color: "white",
+    padding: 10,
+  },
+  Item: {
+    marginTop: 30,
+    padding: 10,
+  },
+  Input: {
+    alignSelf: "flex-start",
 
-    },
-  });
-  
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: 20,
+    marginRight: 5,
+  },
+  Label: {
+    color: "#BB8FCE",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: 20,
+    padding: 10,
+    marginBottom: 0,
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 100,
+  },
+  Header: {
+    color: "#C4EFFF",
+    fontFamily: "Dosis",
+    fontSize: 40,
+    fontWeight: "600",
+    alignSelf: "flex-start",
+  },
+  H2: {
+    textAlign: "center",
+    color: "#BB8FCE",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: 25,
+    marginTop: 5,
+  },
+  Button: {
+    alignSelf: "center",
+    backgroundColor: "#BB8FCE",
+    marginTop: 20,
+  },
+  Text: {
+    textAlign: "center",
+    color: "#BB8FCE",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    fontSize: 25,
+    marginTop: 5,
+  },
+  Textarea: {
+    textAlign: "left",
+    fontFamily: "Dosis",
+    fontWeight: "400",
+    borderRadius: 20,
+    fontSize: 20,
+    padding: 5,
+
+  },
+  modal: {
+    height: 300,
+    width: 300
+  }
+});
